81f28e7b78bb22800dc6c2a60dc15fd1abf8755e
Jan-Philipp <kenste@outlook.de>
2024-05-22 16:16:09 +0200

    feat: initial rune class

---
+++ src/RuneScorer/rune.py
@@ -0,0 +1,61 @@
+import constants
+import math
+import logging
+from weight import WeightProfile
+
+
+class RuneStat:
+    def __init__(self, stat: constants.Stat, value: int):
+        self.stat = stat
+        self.value = value
+
+    def score(self, profile: WeightProfile, innate=False, main=False) -> float:
+        """
+        Returns the score for this rune stat.
+        :param profile: the weight profile to use for the score calculation
+        :param innate: whether this stat is an innate stat or not
+        :return: the score of this rune stat
+        """
+        if innate:
+            res = self.value * profile.innate_weights.get(self.stat)
+            logging.info(f"{self.stat} is scored as innate with weight {profile.innate_weights.get(self.stat)} and result {res}")
+            return res
+
+        avg_roll_count = self.get_roll_count()
+        if not main and avg_roll_count >= 4:
+            roll_count_scale = profile.quad_roll_scale
+        elif not main and avg_roll_count >= 3:
+            roll_count_scale = profile.triple_roll_scale
+        else:
+            roll_count_scale = 1
+
+        weight = profile.stat_weights.get(self.stat)
+        res = self.value * weight * roll_count_scale
+        logging.info(f"{self.stat} is scored with weight {profile.innate_weights.get(self.stat)}, roll scale {roll_count_scale} and result {res}")
+        return res
+
+    def get_roll_count(self) -> int:
+        """
+        Returns the number of average upgrade rolls that went into this stat.
+        :return: the number of upgrade rolls that went into this stat
+        """
+        upgrade_range = constants.sub_upgrade_range.get(self.stat)
+        avg = sum(upgrade_range) / len(upgrade_range)
+        return math.floor(self.value / avg) - 1
+
+
+class Rune:
+    def __init__(self, main: RuneStat, innate: RuneStat, subs: [RuneStat], level: int, slot: int,
+                 quality: constants.Quality):
+        self.main = main
+        self.innate = innate
+        self.subs = subs
+        self.level = level
+        self.slot = slot
+        self.quality = quality
+
+    def score(self, profile):
+        main_score = self.main.score(profile, main=True)
+        innate_score = self.innate.score(profile, innate=True)
+        sub_scores = [sub.score(profile) for sub in self.subs]
+        return main_score + innate_score + sum(sub_scores)
