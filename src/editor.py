cc742cd221b24ee34d0eef0b02ba90e1ea4d584d
Jan-Philipp <kenste@outlook.de>
2024-05-27 23:53:06 +0200

    feat: added missing callbacks for ui elements

--- src/editor.py
+++ src/editor.py
@@ -1,4 +1,5 @@
 import pygubu
+import logging
 
 
 class Editor:
@@ -10,6 +11,32 @@ class Editor:
 
     def run(self):
         self.mainwindow.mainloop()
+
+    def add_profile_clicked(self):
+        # TODO: missing implementation
+        logging.error(f"add_profile_clicked not yet implemented!")
+
+    def save_json_clicked(self):
+        # TODO: missing implementation
+        logging.error(f"save_json_clicked not yet implemented!")
+
+    def load_json_clicked(self):
+        # TODO: missing implementation
+        logging.error(f"load_json_clicked not yet implemented!")
+
+    def validate_float_entry(self, new_value):
+        if new_value == "":
+            return True
+        try:
+            float(new_value)
+            return True
+        except ValueError:
+            return False
+
+    def on_combobox_selected(self):
+        # TODO: missing implementation
+        logging.error(f"on_combobox_selected not yet implemented!")
+
 
 if __name__ == '__main__':
     editor = Editor()
