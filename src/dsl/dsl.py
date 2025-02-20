1b1ea690f9979e544a2af9eb8488ba09c05b33da
Jan-Philipp <kenste@outlook.de>
2025-02-20 19:21:05 +0100

    feat: more testing

--- src/dsl/dsl.py
+++ src/dsl/dsl.py
@@ -1,13 +1,13 @@
 import attributes
 import operators
 import stats
-from logic import AND, OR, NOT
 from runescorer.rune import Rune, RuneStat
 from src.runescorer import constants
 
 AND = operators.AND
 OR = operators.OR
 NOT = operators.NOT
+AtLeast = operators.AtLeast
 
 Level = attributes.Level()
 Legend = attributes.Quality() == constants.Quality.Legend
@@ -168,12 +168,112 @@ if __name__ == '__main__':
     assert not set_filter_fail1(rune)
     assert not set_filter_fail2(rune)
 
+    # operators
+    ## and
+    func1_called = False
+    func2_called = False
+    def test_func1(_):
+        global func1_called
+        func1_called = True
+        return False
+    def test_func2(_):
+        global func2_called
+        func2_called = True
+        return True
+    and_fail = AND(
+        test_func1,
+        test_func2,
+    )
+    and_success = AND(
+        lambda _: True,
+        lambda _: True,
+        lambda _: True
+    )
+    assert and_success(rune)
+    assert not and_fail(rune)
+    # short-circuit
+    assert func1_called
+    assert not func2_called
+
+    ## or
+    func1_called = False
+    func2_called = False
+    or_success = OR(
+        test_func2,
+        test_func1,
+    )
+    or_fail = OR(
+        lambda _: False,
+        lambda _: False,
+        lambda _: False
+    )
+    assert or_success(rune)
+    assert not or_fail(rune)
+    # short-circuit
+    assert func2_called
+    assert not func1_called
+
+    ## not
+    assert NOT(lambda _: False)(rune)
+    assert not NOT(lambda _: True)(rune)
+
+    ## atleast
+    func1_called = False
+    func2_called = False
+    func3_called = False
+    func4_called = False
+    func5_called = False
+    def test_func3(_):
+        global func3_called
+        func3_called = True
+        return True
+    def test_func4(_):
+        global func4_called
+        func4_called = True
+        return True
+    def test_func5(_):
+        global func5_called
+        func5_called = True
+        return True
+
+    atleast_success = AtLeast(3,
+                              test_func1,
+                              test_func2,
+                              test_func3,
+                              test_func4,
+                              test_func5,
+                              )
+    atleast_fail = AtLeast(5,
+                              test_func1,
+                              test_func2,
+                              test_func3,
+                              test_func4,
+                              test_func5,
+                              )
+    assert atleast_success(rune)
+    # short-circuit
+    assert func1_called
+    assert func2_called
+    assert func3_called
+    assert func4_called
+    assert not func5_called
+    assert not atleast_fail(rune)
+
 
     # combination
     some_main_filter = OR(Main.HP_P, Main.ATK_P)
     some_innate_filter = Innate.ATK >= 5
     some_subs_filter = AND(SPD > 4, ATK_P <= 10, ATK_P > 9)
     some_level_filter = AND(Level >= 4, Level < 5)
-    dsl_filter_success = AND(some_main_filter, some_innate_filter, some_subs_filter, some_level_filter)
+    some_stars_filter = NOT(Stars < 6)
+
+    dsl_filter_success = AND(
+        some_main_filter,
+        some_innate_filter,
+        some_subs_filter,
+        some_level_filter,
+        some_stars_filter
+    )
 
     assert dsl_filter_success(rune)
+    assert not AND(dsl_filter_success, ACC > 1000)(rune)
