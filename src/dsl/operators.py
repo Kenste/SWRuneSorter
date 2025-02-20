89b51843e3783a4741855a7cfb32b0099227bbc6
Jan-Philipp <kenste@outlook.de>
2025-02-20 19:20:46 +0100

    feat: AtLeast operator

--- src/dsl/operators.py
+++ src/dsl/operators.py
@@ -11,3 +11,13 @@ def OR(*conditions: Callable[[Rune], bool]):
 
 def NOT(condition: Callable[[Rune], bool]):
     return lambda rune: not condition(rune)
+
+def AtLeast(n: int, *conditions: Callable[[Rune], bool]) -> Callable[[Rune], bool]:
+    def check(rune):
+        count = 0
+        for condition in conditions:
+            count += condition(rune)
+            if count >= n:
+                return True  # Short-circuit when enough conditions are met
+        return False  # Not enough conditions met
+    return check
