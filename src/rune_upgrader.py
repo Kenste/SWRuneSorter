70de80ebcc975a52e9d57766b962293a6c530f98
Jan-Philipp <kenste@outlook.de>
2025-05-15 18:00:39 +0200

    feat: prepare for different preprocessing steps in case scan fails

--- src/rune_upgrader.py
+++ src/rune_upgrader.py
@@ -1,24 +1,9 @@
 import json
-import sys
-import time
-import traceback
 
+from src.util.upgrader.rune_scanner import RuneNotReadableException
 from user_filter import rune_filter
 from util.upgrader.navigator import Navigator
 from util.upgrader.rune_scanner import Scanner
-
-
-def read_rune(scanner: Scanner):
-    try:
-        return scanner.scan_rune()
-    except KeyError as k:
-        print(f"Error while reading the Rune. Could not correctly read {k}!")
-    except AttributeError:
-        # Ignore as there is most likely no rune shown
-        pass
-    except Exception:
-        traceback.print_exc()
-    return None
 
 
 def main():
@@ -34,7 +19,10 @@ def main():
         navigator.reset_rune_i()
         while iterations < len(data["runes_row"]):
             navigator.open_next_rune()
-            rune = read_rune(scanner)
+            try:
+                rune = scanner.scan_rune()
+            except RuneNotReadableException:
+                break
             if rune is None:
                 navigator.increment_rune_i()
                 iterations += 1
@@ -58,7 +46,10 @@ def main():
                     navigator.sell_rune()
             else:
                 navigator.upgrade_rune(9)
-                rune = read_rune(scanner)
+                try:
+                    rune = scanner.scan_rune()
+                except RuneNotReadableException:
+                    break
                 print(rune)
                 if not rune_filter(rune):
                     print("Sell - Did not pass filter!")
@@ -68,7 +59,10 @@ def main():
                         navigator.sell_rune()
                 else:
                     navigator.upgrade_rune(12)
-                    rune = read_rune(scanner)
+                    try:
+                        rune = scanner.scan_rune()
+                    except RuneNotReadableException:
+                        break
                     print(rune)
                     if not rune_filter(rune):
                         print("Sell - Did not pass filter")
@@ -77,7 +71,7 @@ def main():
                         print("KEEP!")
                         kept_runes.append(rune)
             print("--------------------\n")
-    print("\n"*5)
+    print("\n" * 5)
     print("Kept Runes:")
     for rune in kept_runes:
         print(rune)
